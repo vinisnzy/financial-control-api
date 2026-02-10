12d7f8c31415405674c14528d49b5309771391e0
Vinícius de Paula <manoseilacara123@gmail.com>
2026-02-09 22:37:14 -0300

    feat: create income entity and your unit test

---
+++ src/entities/income/income.spec.ts
@@ -0,0 +1,37 @@
+import { randomUUID } from 'node:crypto'
+import { describe, expect, it } from 'vitest'
+import { Income } from './income.js'
+
+describe('Income entity', () => {
+	it('should be create an income', () => {
+		const income = new Income({
+			id: randomUUID().toString(),
+			month: '2026-02',
+			amount: 2000.0,
+		})
+
+		expect(income).toBeInstanceOf(Income)
+		expect(income.month).toEqual('2026-02')
+		expect(income.amount).toEqual(2000.0)
+	})
+
+	it('should not be able to create a income with negative amount', () => {
+		expect(() => {
+			return new Income({
+				id: randomUUID().toString(),
+				month: '2026-02',
+				amount: -2000.0,
+			})
+		}).toThrow()
+	})
+
+	it('should not be able to create a income with amount equal to zero', () => {
+		expect(() => {
+			return new Income({
+				id: randomUUID().toString(),
+				month: '2026-02',
+				amount: 0,
+			})
+		}).toThrow()
+	})
+})
---
+++ src/entities/income/income.ts
@@ -0,0 +1,28 @@
+interface IncomeProps {
+	id: string
+	month: string
+	amount: number
+}
+
+export class Income {
+	private props: IncomeProps
+
+	get id() {
+		return this.props.id
+	}
+
+	get month() {
+		return this.props.month
+	}
+
+	get amount() {
+		return this.props.amount
+	}
+
+	constructor(props: IncomeProps) {
+		if (props.amount <= 0) {
+			throw new Error('Income amount cannot be negative or zero')
+		}
+		this.props = props
+	}
+}
