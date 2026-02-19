113b8981a29731b11f509f54cdd8a8b7017626d8
Vinícius de Paula <manoseilacara123@gmail.com>
2026-02-19 12:06:15 -0300

    feat: create app and main file: server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
-console.log('Hello, World!')
+import { buildApp } from './app.js'
+
+const app = buildApp()
+
+const port = Number(process.env.PORT) || 3333
+
+app.listen({ port }).then(() => {
+	console.log(`🔥 Http server running on port: ${port} 🔥`)
+})
