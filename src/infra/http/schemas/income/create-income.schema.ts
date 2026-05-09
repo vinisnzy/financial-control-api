293def4a56949598f3560c8b6b6292706e19af56
Vinícius de Paula Moraes <viniciusmoraes295651@gmail.com>
2026-05-09 11:09:38 -0300

    fix(create-income-schema): change month and year format on error message

--- src/infra/http/schemas/income/create-income.schema.ts
+++ src/infra/http/schemas/income/create-income.schema.ts
@@ -2,7 +2,7 @@ import z from 'zod'
 
 export const createIncomeSchema = z.object({
 	name: z.string().min(1),
-	month: z.string().regex(/^(?:\d{4})-(?:0[1-9]|1[0-2])$/, 'O mês e ano devem estar no formato: YYYY-DD'),
+	month: z.string().regex(/^(?:\d{4})-(?:0[1-9]|1[0-2])$/, 'O mês e ano devem estar no formato: YYYY-MM'),
 	amount: z.number().positive(),
 })
 
