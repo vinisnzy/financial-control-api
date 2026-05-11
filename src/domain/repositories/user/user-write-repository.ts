0d32da34c84bef63ebb21de54dfaf62ccb5d84a8
Vinícius de Paula Moraes <viniciusmoraes295651@gmail.com>
2026-05-11 08:45:26 -0300

    fix(user-repository): remove userId param from save

--- src/domain/repositories/user/user-write-repository.ts
+++ src/domain/repositories/user/user-write-repository.ts
@@ -2,7 +2,7 @@ import type { User } from '@/domain/entities/user/user.js'
 import type { CreateUserInput } from './dtos/create-user-input-dto.js'
 
 export interface UserWriteRepository {
-	save(user: User, userId: string): Promise<void>
+	save(user: User): Promise<void>
 	create(data: CreateUserInput): Promise<void>
 	delete(id: string): Promise<void>
 }
