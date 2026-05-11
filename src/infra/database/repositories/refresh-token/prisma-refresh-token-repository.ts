d0cdca487d25f96a7d7487d2b8f9148f7e09a226
Vinícius de Paula Moraes <viniciusmoraes295651@gmail.com>
2026-05-11 09:20:47 -0300

    feat: create prisma refresh token repository

---
+++ src/infra/database/repositories/refresh-token/prisma-refresh-token-repository.ts
@@ -0,0 +1,33 @@
+import { httpErrors } from '@fastify/sensible'
+import type { RefreshToken } from '@/domain/entities/refresh-token/refresh-token.js'
+import type { CreateRefreshTokenInput } from '@/domain/repositories/refresh-token/dtos/create-refresh-token-input-dto.js'
+import type { RefreshTokenRepository } from '@/domain/repositories/refresh-token/refresh-token-repository.js'
+import { prisma } from '../../lib/prisma.js'
+import { refreshTokenPrismaToEntity } from '../../mapper/refresh-token-prisma-to-entity.js'
+
+export class PrismaRefreshTokenRepository implements RefreshTokenRepository {
+	async findByToken(token: string): Promise<RefreshToken | null> {
+		const refreshToken = await prisma.refreshToken.findUnique({
+			where: { token },
+		})
+		if (!refreshToken) {
+			return null
+		}
+		return refreshTokenPrismaToEntity(refreshToken)
+	}
+	async findByUserId(userId: string): Promise<RefreshToken[]> {
+		const refreshTokens = await prisma.refreshToken.findMany({
+			where: { userId },
+		})
+		return refreshTokens.map((rt) => refreshTokenPrismaToEntity(rt))
+	}
+	async create(data: CreateRefreshTokenInput): Promise<void> {
+		await prisma.refreshToken.create({ data })
+	}
+	async delete(id: string): Promise<void> {
+		const result = await prisma.refreshToken.delete({ where: { id } })
+		if (!result) {
+			throw httpErrors.notFound(`Refresh token not found with id: ${id}`)
+		}
+	}
+}
