16c3c9ed4a8c4bd0ef458fef80e27810cbfffa8e
Vinícius de Paula Moraes <viniciusmoraes295651@gmail.com>
2026-06-07 17:34:52 -0300

    feat: create middleware for authenticate routes

--- src/infra/http/routes/routes.ts
+++ src/infra/http/routes/routes.ts
@@ -1,12 +1,16 @@
 import type { FastifyInstance } from 'fastify'
+import { authenticate } from '../middlewares/authenticate.js'
 import { authRoutes } from './auth.routes.js'
 import { fixedExpensesRoutes } from './fixed-expenses.routes.js'
 import { incomeRoutes } from './incomes.routes.js'
 import { variableExpensesRoutes } from './variable-expenses.routes.js'
 
 export function registerRoutes(app: FastifyInstance) {
+	app.register(authRoutes, { prefix: '/auth' })
+
+	app.addHook('preHandler', authenticate) // Authenticate the routes below
+
 	app.register(incomeRoutes, { prefix: '/incomes' })
 	app.register(fixedExpensesRoutes, { prefix: '/fixed-expenses' })
 	app.register(variableExpensesRoutes, { prefix: '/variable-expenses' })
-	app.register(authRoutes, { prefix: '/auth' })
 }
